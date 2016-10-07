f1823e7965f3923ad321d50ef208aae4d1b3b182
Seth <naturewonder3000@gmail.com>
2016-10-07 18:15:27 -0400

    Create KVPS.js

---
+++ KVPS.js
@@ -0,0 +1,57 @@
+function KVP_System(Protected){
+  // Generate KVP System via modular pattern (to protect the data)
+  var tempObj = (function() {
+    var data = {};
+    var set = function(key, value) {
+      data[key] = value;
+    }
+    var get = function(key) {
+      if (data[key]) {
+        return data[key];
+      } else {
+        return null;
+      }
+    }
+    var has = function(key) {
+      if (data[key]) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    var clear = function() {
+      data = {};
+    }
+    var Length = function() {
+      return data.length;
+    }
+    if (!Protected) {
+      var all = function() {
+        var d = data;
+        return d;
+      }
+    }
+    if (Protected) {
+      return {
+        set: set,
+        get: get,
+        has: has,
+        Length: Length,
+        clear: clear
+      }
+    } else {
+      return {
+        set: set,
+        get: get,
+        has: has,
+        Length: Length,
+        all: all,
+        clear: clear
+      }
+    }
+  })();
+  //Load properties into the object being constructed
+  for (var prop in tempObj) {
+    this[prop] = tempObj[prop];
+  }
+}
